d262093c677a8cc6b787ab282a2741720d735dda
付辉 <1520107395@qq.com>
2023-01-21 09:26:51 +0800

    ast entrance

--- parser.go
+++ parser.go
@@ -1,67 +1,96 @@
 package ast_assist
 
 import (
-	"github.com/think-next/ast-assist/internal"
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
 	"go/ast"
+	"go/parser"
+	"go/token"
+	"io"
+	"io/ioutil"
 )
 
-type DeclPair struct {
-	Name string
-	Type string
+type CodeParser struct {
+	fileSet  *token.FileSet
+	fileNode *ast.File
+	buf      *bytes.Buffer
+	file     string
 }
 
-// A FuncDecl represents a function declaration
-type FuncDecl struct {
-	Name     string
-	Receiver DeclPair
-	Param    []DeclPair
-	Result   []DeclPair
-
-	root *ast.FuncDecl
+func (c *CodeParser) GetFileSet() *token.FileSet {
+	return c.fileSet
 }
 
-func getFuncDecl(name string, file *ast.File) FuncDecl {
-	funcDecl := FuncDecl{}
+func (c *CodeParser) GetFileNode() *ast.File {
+	return c.fileNode
+}
 
-	ast.Inspect(file, func(node ast.Node) bool {
-		switch t := node.(type) {
-		case *ast.FuncDecl:
-			funcDecl.Name = t.Name.Name
-			funcDecl.root = t
+func (c *CodeParser) GetBuf() *bytes.Buffer {
+	return c.buf
+}
 
-			if t.Recv != nil {
-				field := t.Recv.List[0]
-				for _, v := range field.Names {
-					funcDecl.Receiver.Name = v.Name
-					break
-				}
-				funcDecl.Receiver.Type = internal.TypeExprStr(field.Type)
-			}
+func NewCodeParser(file string, src interface{}) (*CodeParser, error) {
+	parser := &CodeParser{
+		buf: bytes.NewBuffer([]byte{}),
+	}
 
-			if t.Type.Params != nil {
-				for _, v := range t.Type.Params.List {
-					for _, iv := range v.Names {
-						pair := DeclPair{}
-						pair.Name = iv.Name
-						pair.Type = internal.TypeExprStr(v.Type)
-						funcDecl.Param = append(funcDecl.Param, pair)
-					}
-				}
-			}
-
-			if t.Type.Results != nil {
-				for _, v := range t.Type.Results.List {
-					for _, iv := range v.Names {
-						pair := DeclPair{}
-						pair.Name = iv.Name
-						pair.Type = internal.TypeExprStr(v.Type)
-						funcDecl.Result = append(funcDecl.Result, pair)
-					}
-				}
-			}
+	if file != "" {
+		content, err := ioutil.ReadFile(file)
+		if err != nil {
+			return nil, err
 		}
-		return true
-	})
+		parser.buf = bytes.NewBuffer(content)
+		parser.file = file
+	} else {
 
-	return funcDecl
+		var srcBytes []byte
+		if src != nil {
+			switch s := src.(type) {
+			case string:
+				srcBytes = []byte(s)
+			case []byte:
+				srcBytes = s
+			case *bytes.Buffer:
+				// is io.Reader, but src is already available in []byte form
+				if s != nil {
+					srcBytes = s.Bytes()
+				}
+			case io.Reader:
+				srcBytes, _ = io.ReadAll(s)
+			default:
+				return nil, errors.New(fmt.Sprintf("debug info: invalid source type(%T)", src))
+			}
+
+			parser.buf = bytes.NewBuffer(srcBytes)
+		}
+	}
+
+	if err := parser.init(file, src); err != nil {
+		return nil, err
+	}
+
+	return parser, nil
+}
+
+func (c *CodeParser) init(filename string, src interface{}) error {
+	fSet := token.NewFileSet()
+	fNode, err := parser.ParseFile(fSet, filename, src, parser.ParseComments)
+	if err != nil {
+		// Print out the bad code with line numbers.
+		// This should never happen in practice, but it can while changing generated code
+		// so consider this a debugging aid.
+		var src bytes.Buffer
+
+		s := bufio.NewScanner(bytes.NewReader(c.buf.Bytes()))
+		for line := 1; s.Scan(); line++ {
+			fmt.Fprintf(&src, "%5d\t%s\n", line, s.Bytes())
+		}
+		return fmt.Errorf("debug info: %v unparsable code source: %v\n%v", filename, err, c.buf.String())
+	}
+
+	c.fileSet = fSet
+	c.fileNode = fNode
+	return nil
 }
